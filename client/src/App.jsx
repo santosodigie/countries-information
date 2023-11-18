cc0f0d1807821662351c07f30d10cc7faaaf52a3
Santos Odigie <santosodigie104@gmail.com>
2023-11-18 14:10:42 +0000

    feat(form): create input form for country selection
    style(form): style the country input form with CSS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from "react";
-import InputForm from "./inputform";
+import InputForm from "./InputForm";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <Routes>
-        <Route exact path="/" element={<InputForm />} />
-      </Routes>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<InputForm />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- client/src/InputForm.jsx
+++ client/src/InputForm.jsx
@@ -1,23 +1,19 @@
 import React, { useState } from "react";
 
-const [inputValue, setInputValue] = useState("");
-
 function InputForm() {
   return (
-    <>
-      <div className="wrap">
-        <div className="search">
-          <input
-            type="text"
-            className="searchTerm"
-            placeholder="What are you looking for?"
-          />
-          <button type="submit" className="searchButton">
-            <i className="fa fa-search"></i>
-          </button>
-        </div>
+    <div className="wrap">
+      <div className="search">
+        <input
+          type="text"
+          className="searchTerm"
+          placeholder="What are you looking for?"
+        />
+        <button type="submit" className="searchButton">
+          <i className="fa fa-search"></i>
+        </button>
       </div>
-    </>
+    </div>
   );
 }
 
